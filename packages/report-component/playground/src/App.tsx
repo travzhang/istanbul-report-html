2c2eb06ced594b578530ac3af4dbb10518c00f81
Travis Zhang <wr.zhang25@gmail.com>
2026-08-24 16:28:53 +0800

    feat: update

--- packages/report-component/playground/src/App.tsx
+++ packages/report-component/playground/src/App.tsx
@@ -1,8 +1,25 @@
 import { MyButton } from '../../src'
+// main.ts (先 import 上面那个文件，确保 MonacoEnvironment 提前挂载)
+import './monaco-workers'
+import * as monaco from 'monaco-editor'
+import {useEffect} from "react";
+
 
 export function App() {
+  useEffect(() => {
+
+    if (document.getElementById('editor')) {
+      const container = document.getElementById('editor')!
+      monaco.editor.create(container, {
+        value: 'console.log("hello world")',
+        language: 'javascript',
+        theme: 'vs-dark',
+      })
+    }
+  }, []);
   return (
     <>
+      <div id="editor" style={{ height: '500px',width:'750px' }}></div>
       <MyButton type="primary" />
     </>
   )
